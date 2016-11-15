8d19e8ab9ad010bf8a803e136d9bdcb95b545173
Griffin <gtb115@gmail.com>
2016-11-14 19:49:03 -0800

    more map shit

--- public/js/homepage/maps.js
+++ public/js/homepage/maps.js
@@ -1,5 +1,7 @@
 //declare map outside of functions incase you want to use it somewhere else.
 var map;
+var markers;
+var markerCluster;
 
 function initMap()
 {
@@ -15,60 +17,68 @@ function initMap()
 function initialize(position)
 {
 	//create var to use later.  populate with info from navigator
-	var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
-
+	//var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
+  var currentLocation = {lat: 37.832685, lng: -122.273709};
 	//set any map options here
 	var mapOptions = {
 		center: currentLocation,
-		zoom: 16
+		zoom: 15
 	};
 
 	//find the id="map" and place a map within it.  pass in any map options
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-
-  	var infoWindow = new google.maps.InfoWindow({map: map});
+	//init info window.  not used for anything yet
+  	//var infoWindow = new google.maps.InfoWindow({map: map});
   	//infoWindow.setPosition(currentLocation);
 
 
-  	var markers = [];
-  	for (var i = 0; i < locations.length; i++) {
-  		markers.push(addMarkerWithTimeout(locations[i], i * 500));
-    }
-    console.log('markers');
-    console.log(markers);
-
-    function addMarkerWithTimeout(position, timeout) {
-        window.setTimeout(function() {
-          	markers.push(new google.maps.Marker({
-            	position: position,
-            	map: map,
-            	animation: google.maps.Animation.DROP
-        	}));
-        }, timeout);
-    }
-
-    function clearMarkers() {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-        }
-        markers = [];
-      }
-/*
-	//add some markers to the map.  be sure to define locations prior to this code
-	var markers = locations.map(function(location, i)
-	{
-        return new google.maps.Marker({
-        	position: location,
-            label: i,
-            animation: google.maps.Animation.DROP,
-            map: map
-        });
-    });
-*/
-    //add a marker clusterer to manage the markers.
-    var markerCluster = new MarkerClusterer(map, markers, {
-    	imagePath: 'images/m'});
+  	//function to drop markers.  clustering is the callback function
+  	dropMarkers(markers);
 }
+
+        function dropMarkers(markers) {
+        	//locations are passed in from php json.  variable is defined/populated prior to this .js file.
+        	let test = new Promise(function(resolve, reject) {
+            markers = locations.map(addMarkerWithTimeout);
+            
+            resolve(markers);
+          });
+
+          test.then(function(value){
+            console.log('markers before clustering');
+
+            //console dump is showing that markers are being populated correctly.  I believe the problem is with the .push method above
+            console.log(value);
+            clustering(value);
+          });
+        }
+
+
+        function addMarkerWithTimeout(position, timeout) {
+          let prom = new Promise(function(resolve, reject) {
+            window.setTimeout(function(){
+              resolve(new google.maps.Marker({
+                      position: position,
+                      map: map,
+                      animation: google.maps.Animation.DROP}));
+            }, timeout*300);
+          });
+
+          prom.then(function(value) {
+            console.log('iteration: [' + timeout + '] value: [' + value +']');
+            return value;
+          });
+        }
+
+
+        //add a marker clusterer to manage the markers.
+        function clustering(markers) {
+        	console.log('markers after callback');
+        	console.log(markers);
+
+        	markerCluster = new MarkerClusterer(map, markers, {
+        	imagePath: 'images/m'});
+        }
 
 
